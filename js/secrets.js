0b6bfc2f0fe89c05d10bc33a7d5af3a38bad6113
ClassicalExplosion <85848810+flybot2010-lab@users.noreply.github.com>
2025-03-07 18:51:42 -0600

    Update secrets.js

--- js/secrets.js
+++ js/secrets.js
@@ -1,2 +1,3 @@
 var api_key = 'e1f10a1e78da46f5b10a1e78da96f525'    
 var map_key = 'pk.eyJ1Ijoid2VhdGhlciIsImEiOiJjamxnc3NsbHExYXkyM3FwYWllN3FhYnZ3In0.mX6H3_kilkaK01_Q-Htz3A'
+var apiKeyGoogle = 'temp'
